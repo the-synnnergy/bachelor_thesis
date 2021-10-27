4f7b86193165e6065db0dad23caf033deb1de725
Marcel Dokters <mdokters@uni-osnabrueck.de>
2021-10-27 22:36:55 +0200

    Added class base for IndexStats structure

---
+++ src/main/java/IndexStats/IndexStats.java
@@ -0,0 +1,35 @@
+package IndexStats;
+
+import org.apache.lucene.index.IndexReader;
+
+import java.util.Map;
+
+public class IndexStats {
+    IndexStats(IndexReader reader){
+
+    }
+
+    public Map<String,Double> get_document_termvector(String doctitle){
+
+    };
+
+    public Map<String,Double> get_termfrequency_per_doc(String term){
+
+    }
+
+    public double get_termfreq_doc(String term,String doctitle){
+
+    }
+
+    public long get_corpus_termfrequency(String term){
+
+    }
+
+    public int doc_length(String doctitle){
+
+    }
+
+    public
+
+
+}
