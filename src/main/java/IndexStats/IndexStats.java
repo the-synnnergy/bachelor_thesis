ecbeaacdede9846c90bb1f0e0deef7c6aa5d0948
Marcel Dokters <mdokters@uni-osnabrueck.de>
2021-10-31 20:16:44 +0100

    Adding more to IndexStats, so codebase will be better

--- src/main/java/IndexStats/IndexStats.java
+++ src/main/java/IndexStats/IndexStats.java
@@ -1,22 +1,43 @@
 package IndexStats;
 
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
 
+import java.io.IOException;
 import java.util.Map;
+
+import static org.apache.lucene.search.DocIdSetIterator.NO_MORE_DOCS;
 
 public class IndexStats {
 
      private Map<Integer,Integer> doc_length_map;
      private Map<Integer,Map<String,Double>> term_probs_per_doc;
      private Map<String,Double> corpus_probs;
-
-    IndexStats(IndexReader reader){
-
+     private Map<String,Long> corpus_termfrequency;
+     private Map<String,Double> term_idf_map;
+     private Map<Integer,Map<String,Integer>> doc_tf_map;
+    IndexStats(IndexReader reader) throws IOException {
+        Terms all_terms = MultiTerms.getTerms(reader,"body");
+        TermsEnum all_terms_it = all_terms.iterator();
+        while (all_terms_it.next() != null){
+            String term = all_terms_it.term().utf8ToString();
+            corpus_termfrequency.put(term,all_terms_it.totalTermFreq());
+            term_idf_map.put(term, (double) new ClassicSimilarity().idf(all_terms_it.docFreq(),reader.numDocs()));
+            PostingsEnum postings = all_terms_it.postings(null);
+            int doc_id = 0;
+            while ((doc_id =postings.nextDoc()) != NO_MORE_DOCS){
+                // Add frequencies to Map key for document length! #TODO fix this to readable!
+                doc_length_map.put(doc_id, doc_length_map.getOrDefault(doc_id,0)+ postings.freq());
+                //doc_tf_map.put(doc_id,postings.freq());
+            }
+        }
     }
 
-    public Map<String,Double> get_document_termvector(String doctitle){
+    public Map<String,Double> get_document_termmap(String doctitle){
         return null;
     };
+
+    public double[] get_document_termvector(String doctile){ return null;}
 
     public Map<String,Double> get_termfrequency_per_doc(String term){
         return null;
