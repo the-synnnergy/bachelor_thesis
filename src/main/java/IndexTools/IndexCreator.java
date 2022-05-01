56efde24eb8e0062696ae5dde677143db9096306
Marcel Dokters <mdokters@uni-osnabrueck.de>
2022-05-01 21:42:30 +0200

    Added IndexCreator and Tests

--- src/main/java/IndexTools/IndexCreator.java
+++ src/main/java/IndexTools/IndexCreator.java
@@ -1,30 +1,38 @@
 package IndexTools;
 
 import FeaturesCalc.FeaturesCalc;
-import FeaturesCalc.SimilarityCalc.*;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.StringField;
-import org.apache.lucene.index.*;
-import org.apache.lucene.search.similarities.*;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.search.similarities.LMDirichletSimilarity;
+import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
 public class IndexCreator
 {
-    public static String[] createIndices(String index_basepath, Map<String,String> name_to_content) throws IOException
+    public static String[] createIndices(String index_basepath, Map<String, String> name_to_content) throws IOException
     {
         String[] index_paths = new String[FeaturesCalc.Similarities.values().length];
-        for(FeaturesCalc.Similarities sim : FeaturesCalc.Similarities.values())
+        for (FeaturesCalc.Similarities sim : FeaturesCalc.Similarities.values())
         {
-            index_paths[sim.ordinal()] = createIndex(sim, index_basepath,name_to_content);
+            index_paths[sim.ordinal()] = createIndex(sim, index_basepath, name_to_content);
         }
         return index_paths;
     }
@@ -33,9 +41,9 @@ public class IndexCreator
     {
         IndexWriterConfig indexWriterConfig = new IndexWriterConfig(new EnglishAnalyzer());
         indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
-        String index_path = index_basepath + "/"+sim.name();
+        String index_path = index_basepath + sim.name();
         Directory dir = FSDirectory.open(Paths.get(index_path));
-        switch(sim)
+        switch (sim)
         {
             case JELINEK_MERCER:
                 indexWriterConfig.setSimilarity(new LMJelinekMercerSimilarity(0.7f));
@@ -51,20 +59,32 @@ public class IndexCreator
                 indexWriterConfig.setSimilarity(new LMDirichletSimilarity());
                 break;
         }
-        IndexWriter indexWriter = new IndexWriter(dir,indexWriterConfig);
-        for(Map.Entry<String,String> entry : name_to_content.entrySet())
+        IndexWriter indexWriter = new IndexWriter(dir, indexWriterConfig);
+
+        for (Map.Entry<String, String> entry : name_to_content.entrySet())
         {
-            Document doc = new Document();
-            FieldType body_field_type = new FieldType();
-            body_field_type.setTokenized(true);
-            body_field_type.setStored(true);
-            body_field_type.setStoreTermVectors(true);
-            body_field_type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
-            Field body_field = new Field("body",entry.getValue(),body_field_type);
-            doc.add(body_field);
-            doc.add(new StringField("title",entry.getKey(),Field.Store.YES));
-            indexWriter.addDocument(doc);
+            try
+            {
+                Document doc = new Document();
+                FieldType body_field_type = new FieldType();
+                body_field_type.setTokenized(true);
+                body_field_type.setStored(true);
+                body_field_type.setStoreTermVectors(true);
+                body_field_type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
+                Path file = Path.of(entry.getValue());
+                String body = Files.readString(file, StandardCharsets.ISO_8859_1);
+                Field body_field = new Field("body", body, body_field_type);
+                doc.add(body_field);
+                doc.add(new StringField("title", entry.getKey(), Field.Store.YES));
+                indexWriter.addDocument(doc);
+            } catch (java.nio.charset.MalformedInputException e)
+            {
+                System.out.println("File not found:"+entry.getValue());
+            }
         }
+
+        indexWriter.commit();
+        indexWriter.close();
 
         return index_path;
     }
--- src/main/java/xml_test.java
+++ src/main/java/xml_test.java
@@ -1,10 +1,16 @@
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
+import static IndexTools.IndexCreator.createIndices;
 import static XMLParser.iTrustParser.get_NameFileMap;
 import static XMLParser.iTrustParser.get_true_req_to_source_links;
 
@@ -12,7 +18,16 @@ public class xml_test
 {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException
     {
-        Map<String,String> a = get_NameFileMap(null,"/home/marcel/Downloads/iTrust/source_req.xml");
-        Map<String, List<String>> b = get_true_req_to_source_links("","/home/marcel/Downloads/iTrust/answer_req_code.xml");
+        Map<String,String> name_to_file = get_NameFileMap("/home/marcel/Downloads/iTrust/","/home/marcel/Downloads/iTrust/source_req.xml");
+        String[] index_paths = createIndices("index_test/",name_to_file);
+        Directory dir = FSDirectory.open(Paths.get("./index_test/BM_25"));
+        IndexReader reader = DirectoryReader.open(dir);
+        for (int i = 0; i< reader.numDocs();i++)
+        {
+            System.out.println(reader.document(i).getField("body").stringValue());
+        }
+
+
+        //Map<String, List<String>> b = get_true_req_to_source_links("/home/marcel/Downloads/iTrust/","/home/marcel/Downloads/iTrust/answer_req_code.xml");
     }
 }
